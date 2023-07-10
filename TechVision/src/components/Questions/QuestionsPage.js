22647f3e4df316f6f7119f40538dee689730ae82
Gustavo <125235805+DnzGus@users.noreply.github.com>
2023-07-10 05:00:30 -0300

    Finished

--- TechVision/src/components/Questions/QuestionsPage.js
+++ TechVision/src/components/Questions/QuestionsPage.js
@@ -1,12 +1,12 @@
 const QPage = () => {
     return (<main className="flex flex-col justify-center items-center flex-wrap min-h-screen m-0 bg-sky-950">
         <div className="flex flex-col justify-center items-center m-4 flex-wrap bg-slate-300 rounded-2xl shadow-black border-2 border-slate-700 shadow-xl p-16 gap-4">
-            <a href="#" className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Fale conosco.</a>
-            <a href="#" className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Privacidade.</a>
-            <a href="#" className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Reporte um erro.</a>
-            <a href="#" className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Suporte</a>
-            <a href="#" className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Serviços</a>
-            <a href="#" className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Serviço Digital</a>
+            <button className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Fale conosco.</button>
+            <button className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Privacidade.</button>
+            <button className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Reporte um erro.</button>
+            <button className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Suporte</button>
+            <button className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Serviços</button>
+            <button className="cursor-pointer hover:scale-105 text-red-600 font-semibold">Serviço Digital</button>
         </div>
     </main>)
 }
